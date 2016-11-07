6913ac18bf5eae21278812005cd587d1d6bbeb80
Corey MacBook Pro <coreyearleon@icloud.com>
2016-11-07 09:55:09 -0800

    Initial commit with GitHub project

---
+++ iOS-csr-swift/iOS-csr-swift.h
@@ -0,0 +1,19 @@
+//
+//  iOS-csr-swift.h
+//  iOS-csr-swift
+//
+//  Created by Corey Baker on 11/7/16.
+//  Copyright © 2016 One Degree Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS-csr-swift.
+FOUNDATION_EXPORT double iOS_csr_swiftVersionNumber;
+
+//! Project version string for iOS-csr-swift.
+FOUNDATION_EXPORT const unsigned char iOS_csr_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_csr_swift/PublicHeader.h>
+
+
