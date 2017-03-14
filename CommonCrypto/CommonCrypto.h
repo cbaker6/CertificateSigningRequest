381a142d50b1a466cc38ea47a472addf3e4cfa6a
Corey MacBook Pro <coreyearleon@icloud.com>
2017-03-14 16:42:14 -0700

    Added back CommonCrypto module maps. It seems they are needed.

---
+++ CommonCrypto/CommonCrypto.h
@@ -0,0 +1,19 @@
+//
+//  CommonCrypto.h
+//  CommonCrypto
+//
+//  Created by Corey Baker on 3/14/17.
+//  Copyright © 2017 One Degree Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CommonCrypto.
+FOUNDATION_EXPORT double CommonCryptoVersionNumber;
+
+//! Project version string for CommonCrypto.
+FOUNDATION_EXPORT const unsigned char CommonCryptoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CommonCrypto/PublicHeader.h>
+
+
