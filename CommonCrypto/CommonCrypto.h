a3cb3af5a5c00ee9c1d699d3c3f95e75c9fb5dae
Corey MacBook Pro <coreyearleon@icloud.com>
2017-03-14 17:41:40 -0700

    Still trying to fix cocoa pod

--- CommonCrypto/CommonCrypto.h
+++ CommonCrypto/CommonCrypto.h
@@ -14,6 +14,6 @@ FOUNDATION_EXPORT double CommonCryptoVersionNumber;
 //! Project version string for CommonCrypto.
 FOUNDATION_EXPORT const unsigned char CommonCryptoVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CommonCrypto/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like <CommonCrypto/PublicHeader.h>
 
-
+#import <CommonCrypto/CommonCrypto.h>
