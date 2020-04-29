1e4ade42d177ad1ad0022b255ed267b737d547c9
Corey MacBook Pro <coreyearleon@icloud.com>
2020-04-29 19:35:50 -0400

    fix spm

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CertificateSigningRequest",
-    platforms: [.iOS(.v9.3), .macOS(.v10_12)],
+    platforms: [.iOS(.v9), .macOS(.v10_12)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
