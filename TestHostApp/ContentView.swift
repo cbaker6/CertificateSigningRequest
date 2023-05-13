5007230981492c5053881e9468d5251986355467
Corey <coreyearleon@icloud.com>
2023-05-13 11:22:04 -0400

    fix app name

--- TestHostApp/ContentView.swift
+++ TestHostApp/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  TestApp
+//  TestHostApp
 //
 //  Created by Corey Baker on 5/13/23.
 //
--- TestHostApp/TestHostApp.swift
+++ TestHostApp/TestHostApp.swift
@@ -1,14 +1,15 @@
 //
-//  TestAppApp.swift
-//  TestApp
+//  TestHostApp.swift
+//  TestHost
 //
 //  Created by Corey Baker on 5/13/23.
 //
 
 import SwiftUI
+import CertificateSigningRequest
 
 @main
-struct TestAppApp: App {
+struct TestHostApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
