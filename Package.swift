237a93afa56b716ec6148f83927957edba7b0343
Corey <coreyearleon@icloud.com>
2023-05-13 13:53:45 -0400

    ci: Only use one command to build cocoapods (#68)
    * ci: Fix release
    
    * rename build file
    
    * clean up

--- Package.swift
+++ Package.swift
@@ -5,22 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "CertificateSigningRequest",
-    platforms: [.iOS(.v13), .macCatalyst(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)],
+    platforms: [.iOS(.v13),
+        .macCatalyst(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "CertificateSigningRequest",
-            targets: ["CertificateSigningRequest"])
+        .library(name: "CertificateSigningRequest",
+                 targets: ["CertificateSigningRequest"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        .target(
-            name: "CertificateSigningRequest"),
-        .testTarget(
-            name: "CertificateSigningRequestTests",
-            dependencies: ["CertificateSigningRequest"])
+        .target(name: "CertificateSigningRequest"),
+        .testTarget(name: "CertificateSigningRequestTests",
+                    dependencies: ["CertificateSigningRequest"])
     ]
 )
--- Package@5.0.swift
+++ Package@5.0.swift
@@ -5,22 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "CertificateSigningRequest",
-    platforms: [.iOS(.v10), .macOS(.v10_13), .tvOS(.v10), .watchOS(.v3)],
+    platforms: [.iOS(.v10),
+        .macOS(.v10_13),
+        .tvOS(.v10),
+        .watchOS(.v3)],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "CertificateSigningRequest",
-            targets: ["CertificateSigningRequest"])
+        .library(name: "CertificateSigningRequest",
+                 targets: ["CertificateSigningRequest"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        .target(
-            name: "CertificateSigningRequest"),
-        .testTarget(
-            name: "CertificateSigningRequestTests",
-            dependencies: ["CertificateSigningRequest"])
+        .target(name: "CertificateSigningRequest"),
+        .testTarget(name: "CertificateSigningRequestTests",
+                    dependencies: ["CertificateSigningRequest"])
     ]
 )
